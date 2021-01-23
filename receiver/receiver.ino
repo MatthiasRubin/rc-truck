2a8f6b7a3147d0bf94a2e99b70dffa11a987b8ef
Matthias Knöpfel <rubinknoepfel@bluewin.ch>
2021-01-23 19:13:11 +0100

    Added receiver

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -4,8 +4,8 @@ Servo myservo;
 
 enum
 {
-  joystickXPin = 0,
-  joystickYPin = 1,
+  dataPin = 12,
+  servoPin = 9,
   forewardPin = 2,
   speedPin = 3,
   reversePin = 4
@@ -13,8 +13,6 @@ enum
 
 enum
 {
-  middleJoystickValueX = 507,
-  middleJoystickValueY = 514,
   minSteeringAngle = 40,
   middleSteeringAngle = 108,
   maxSteeringAngle = 160
@@ -22,7 +20,7 @@ enum
 
 void setup()
 {
-  myservo.attach(9);
+  myservo.attach(servoPin);
 
   pinMode(forewardPin, OUTPUT);
   pinMode(speedPin, OUTPUT);
@@ -31,64 +29,99 @@ void setup()
   digitalWrite(forewardPin,LOW);
   digitalWrite(speedPin,LOW);
   digitalWrite(reversePin,LOW);
+
+  Serial.begin(9600);
 }
 
 void loop()
-{ 
-  drive();
-  steer();
+{
+  int8_t driveData = (int8_t)receiveData();
+  int8_t steerData = (int8_t)receiveData();
+  
+  drive(driveData);
+  steer(steerData);
 
   delay(15);
 }
 
-
-void drive()
+uint8_t receiveData()
 {
-  unsigned joystickValue = analogRead(joystickYPin);
-  unsigned speed = 0;
+  unsigned long startTime = millis();
+  bool level = LOW;
+  unsigned long time = 0;
+  uint8_t data = 0;
 
-  if (joystickValue >= middleJoystickValueY)
+  // waiting for start bit
+  do
   {
-    speed = map(joystickValue, middleJoystickValueY, 1023, 0, 255);
+    level = digitalRead(dataPin);
+    time = millis() - startTime;
+  } while ((!level) && (time < 150));
+
+  if (level)
+  {
+    delayMicroseconds(150);
+    
+    // receive data
+    for (int i = 0; i < 8; ++i)
+    {
+      data <<= 1;
+      
+      if (digitalRead(dataPin))
+      {
+        data |= 1;
+      }
+
+      delayMicroseconds(100);
+    }
+  }
+  return data;
+}
+
+
+void drive(int value)
+{
+  unsigned speed;
+
+  if (value >= 0)
+  {
+    speed = map(value, 0, 127, 0, 255);
 
     digitalWrite(reversePin,LOW);
     digitalWrite(forewardPin,HIGH);
+
+    Serial.print(speed);
   }
   else
   {
-    speed = map(joystickValue, 0, middleJoystickValueY, 150, 0);
+    speed = map(value, -128, 0, 150, 0);
     
     digitalWrite(forewardPin,LOW);
     digitalWrite(reversePin,HIGH);
+    
+    Serial.print(-((int)speed));
   }
+  
+  Serial.print(" ");
 
   analogWrite(speedPin,speed);
 }
 
 
-void steer()
+void steer(int value)
 {
-  static unsigned buffer[8] = {0};
-  static unsigned index = 0;
-  static unsigned sum = 0;
+  unsigned angle;
   
-  sum -= buffer[index];
-  buffer[index] = analogRead(joystickXPin);
-  sum += buffer[index];
-  index++;
-  index &= 7;
-  
-  unsigned joystickValue = (sum>>3);
-  unsigned angle = 0;
-  
-  if (joystickValue >= middleJoystickValueX)
+  if (value >= 0)
   {
-    angle = map(joystickValue, middleJoystickValueX, 1023, middleSteeringAngle, maxSteeringAngle);
+    angle = map(value, 0, 127, middleSteeringAngle, maxSteeringAngle);
   }
   else
   {
-    angle = map(joystickValue, 0, middleJoystickValueX, minSteeringAngle, middleSteeringAngle);
+    angle = map(value, -128, 0, minSteeringAngle, middleSteeringAngle);
   }
+  
+  Serial.println(angle);
   
   myservo.write(angle);
 }
